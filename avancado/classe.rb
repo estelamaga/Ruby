e1d985cade7275798782dfda76451452d322e1bb
estelamaga <estelinha.mag@hotmail.com>
2020-01-15 17:55:40 -0300

    Conceito sobre construtores

--- avancado/classe.rb
+++ avancado/classe.rb
@@ -1,6 +1,12 @@
 
 class Conta
-  attr_accessor :saldo
+  attr_accessor :saldo, :nome
+
+  # Método construtor
+  def initialize(nome)
+    self.saldo = 0.0
+    self.nome = nome
+  end
 
   def depositar(valor)
 
@@ -9,11 +15,16 @@ class Conta
     # puts "Depositando a quantia de " + valor.to_s + "reais."
 
     #Interpolação uma forma mais elegante de transformar em string, transformação automatica para string.
-    puts "Depositando a quantia de  #{valor} reais"
+    puts "Depositando a quantia de  #{valor} reais na conta de #{self.nome}."
   end
 end
 
-c = Conta.new
+c = Conta.new("Estela")
 
-c.saldo = 0.0
+# c.saldo = 0.0
 c.depositar(100.00)
+puts c.saldo
+
+c.depositar(10.00)
+puts c.saldo
+puts c.nome
